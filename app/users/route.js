c68dc336baeb223c8a0ade6fb5a17823fe2052ac
Ju Liu <ju.liu@welaika.com>
2016-05-21 23:08:07 +0100

    Convert route to Ember idiomatic way

--- app/users/route.js
+++ app/users/route.js
@@ -1,16 +1,20 @@
 import Ember from 'ember';
-import route from 'ember-redux/route';
-import request from 'ember-ajax/request';
 
-let model = (dispatch) => {
-  return request('https://api.github.com/users').then((response) => {
-    dispatch({
-      type: 'DESERIALIZE_USERS',
-      response: response
+const { inject, get } = Ember;
+
+export default Ember.Route.extend({
+  ajax: inject.service(),
+  redux: inject.service(),
+
+  model() {
+    let ajax = get(this, 'ajax');
+    let redux = get(this, 'redux');
+
+    return ajax.request('https://api.github.com/users').then((response) => {
+      redux.dispatch({
+        type: 'DESERIALIZE_USERS',
+        response: response
+      });
     });
-  });
-};
-
-let UsersRoute = Ember.Route.extend();
-
-export default route({model})(UsersRoute);
+  }
+});
